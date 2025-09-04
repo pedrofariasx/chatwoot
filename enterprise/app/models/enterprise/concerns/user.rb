92ac7370c18b799f511dc836bea2d8db9ec1af19
Pedro Farias <49423605+pedrofariasx@users.noreply.github.com>
2025-09-04 04:43:57 -0300

    Update user.rb

--- enterprise/app/models/enterprise/concerns/user.rb
+++ enterprise/app/models/enterprise/concerns/user.rb
@@ -8,9 +8,8 @@ module Enterprise::Concerns::User
     has_many :copilot_threads, dependent: :destroy_async
   end
 
-  def ensure_installation_pricing_plan_quantity
-    return unless ChatwootHub.pricing_plan == 'premium'
-
-    errors.add(:base, 'User limit reached. Please purchase more licenses from super admin') if User.count >= ChatwootHub.pricing_plan_quantity
-  end
+def ensure_installation_pricing_plan_quantity
+	​if User.count >= 100000     
+	​	​errors.add(:base, 'User limit reached. Please purchase more licenses from super admin')
+	​end 
 end
